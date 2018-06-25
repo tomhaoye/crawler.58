83164136987cbb1a2e4fe063bbad3728b9378dbb
邱奕玮 <qiuyiwei@3ekeji.cn>
2018-06-25 15:56:06 +0800

    10进程同时抓取页面数据

--- app/detail.py
+++ app/detail.py
@@ -4,18 +4,17 @@ from model.community import Community
 import requests
 import logging
 from pyquery import PyQuery
-import time
 import json
+from queue import Queue
+from threading import Thread
 
 db_session = Session()
 
 
-def fetch_detail_page(db, city_index):
-    communities = db.query(Community).filter(
-        Community.city_index == city_index,
-        Community.detail_info == None
-    ).all()
-    for community in communities:
+def thread_fetch(community_queue: Queue):
+    db = Session()
+    while not community_queue.empty():
+        community = community_queue.get()
         logging.info(f'开始抓取{community.alias}小区详情页数据')
         url = f'http://m.58.com/xiaoqu/{community.listname}/'
         try:
@@ -23,6 +22,7 @@ def fetch_detail_page(db, city_index):
             res.raise_for_status()
         except Exception as e:
             logging.error(f' 错误信息: {e}')
+            community_queue.task_done()
             continue
         doc = PyQuery(res.content)
         keys = []
@@ -50,9 +50,29 @@ def fetch_detail_page(db, city_index):
             community.alias = alias
         db.commit()
         logging.info(f'抓取{community.alias}小区数据完成')
-        # time.sleep(1)
-    logging.info(f'抓取{city_index}所有小区数据完成')
+        community_queue.task_done()
+
+
+def fetch_detail_page(db, city_index):
+    communities = db.query(Community).filter(
+        Community.city_index == city_index,
+        Community.detail_info == None
+    ).all()
+    community_count = len(communities)
+    logging.info(f'一共有{community_count}个小区需要抓取详情页数据')
+    community_queue = Queue()
+    for community in communities:
+        community_queue.put(community)
+
     db.close()
+
+    for _ in range(10):
+        worker = Thread(target=thread_fetch, args=[community_queue])
+        worker.start()
+
+    community_queue.join()
+
+    logging.info(f'抓取{city_index}所有小区数据完成')
 
 
 def main(city_index):
